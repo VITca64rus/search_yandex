22eb2bc5be4b053ee6d2c950c57bcf8839e698de
vitca64rus <vitca64rus@gmail.com>
2022-06-30 10:58:44 +0300

    initial

---
+++ route.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask import request
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    find = request.args.get('find')
+    if find is None:
+        return("Передайте get параметр find. Пример 127.0.0.1:3000/?find=фармстандарт")
+    else:
+        driver = webdriver.Chrome()
+        option = Options()
+        option.add_argument("--disable-infobars")
+        browser = webdriver.Chrome(executable_path='/home/sazelda/Downloads/chromedriver_linux64(2)/chromedriver', options=option)
+        browser.get('https://yandex.ru')
+        elem = browser.find_element(By.ID, 'text')
+        elem.send_keys(find + Keys.RETURN)
+        # time.sleep(4)
+        source_data = browser.page_source
+        soup = BeautifulSoup(source_data)
+        title = soup.find_all(attrs={"class": {"OrganicTitleContentSpan organic__title"}})
+        label = soup.find_all(attrs={"class": {"OrganicTextContentSpan"}})
+        src = soup.find_all(attrs={"class": {"Path Organic-Path path organic__path"}})
+
+        res = "<table>" \
+              "<tr>" \
+              "<th>Номер</th>" \
+              "<th>Заголовок</th>" \
+              "<th>Описание</th>" \
+              "<th>Ссылка</th>" \
+              "</tr>"
+        print(len(title),len(label),len(src))
+        for i in range(len(title)):
+            if i == 10:
+                break
+            res+="<tr>"
+            res += "<th>" + str(i+1) + "</th>"
+            res += "<th>"+str(title[i])+"</th>"
+            res += "<th>" + str(label[i]) + "</th>"
+            res += "<th>" + str(src[i]) + "</th>"
+            res += "</tr>"
+        return res
+
+if __name__ == '__main__':
+    app.run(port=3000, debug = True)
